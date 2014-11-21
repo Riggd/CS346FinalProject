fe36f513021d923391ce3dae1d79a4761dedb9f3
Derek <wr3tched@gmail.com>
2014-11-21 13:46:19 -0500

    Results displayed as table

--- www/index.php
+++ www/index.php
@@ -1,28 +1,25 @@
 <html>
-    <head>
-    	<title>CS346 - Cameron Beasley, Derek Onay</title>
-    </head>
-    	
-    <body>
-    	<?php
-            require_once('connect.php');
-        ?>
-    	<h1>Welcome to our final project!</h1>
+<head>
+	<title>CS346 - Cameron Beasley, Derek Onay</title>
+</head>
+	
+<body>
+	<?php require_once('connect.php');?>
+	<h1>Welcome to our final project!</h1>
 
-    	<p>Query 1:</br>
-    	   Choose which table you would like to see all records from:
-    	</p>
+	<p>Query 1:</br>
+	   Choose which table you would like to see all records from:
+	</p>
+	<form action="query1.php" method="POST">
+        <select name="table">
+            <option value='employee'>Employees</option>
+            <option value='student'>Student</option>
+            <option value='student_workers'>Student_Workers</option>
+            <option value='equipment'>Equipment</option>
+            <option value='transactions'>Transactions</option>
+        </select>
+        <input type="submit" value="Find Table">
+    </form>
+</body>
 
-    	<form action="test.php" method="POST">
-            <select name="table">
-                <option value='employee'>Employees</option>
-                <option value='student'>Student</option>
-                <option value='student_workers'>Student_Workers</option>
-                <option value='equipment'>Equipment</option>
-                <option value='transactions'>Transactions</option>
-            </select>
-            <input type="submit" value="Find Table">
-        </form>
-        
-    </body>
 </html>
--- www/query1.php
+++ www/query1.php
@@ -1,18 +1,27 @@
+<a href='index.php'>Back to Home</a>
 <?php
 require_once('connect.php');
 
 if(isset($_POST['table'])) {
     
-    $query = "SELECT DISTINCT * FROM ".$_POST['table']."";
+    $query = "SELECT * FROM ".$_POST['table']."";
 	$response = @mysqli_query($dbc, $query);
 
 	// If the query executed properly proceed
 	if($response){
+		echo "<table>";
+
 		while($rows = mysqli_fetch_array($response, MYSQL_ASSOC)){
+			echo "<tr>";
+		    
 			foreach ($rows as $row) {
-				echo $row . '</br>';
+
+				echo '<td>' . $row . '</td>';
 			}	
+
+			echo "</tr>";
 		}
+		echo "</table";
 	} 
 	else {
 
